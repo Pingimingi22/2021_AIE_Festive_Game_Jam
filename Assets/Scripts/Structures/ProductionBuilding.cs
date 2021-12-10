4f265106d19ab2fe7a5d59d5d935d0de03e1a817
Nicolas Furr <nicolasfurr@optusnet.com.au>
2021-12-10 11:22:03 +1100

    Started production logic

--- Assets/Scripts/Structures/ProductionBuilding.cs
+++ Assets/Scripts/Structures/ProductionBuilding.cs
@@ -13,15 +13,29 @@ public class ProductionBuilding : Structure
     public float m_CurrentProgress;
     public Item m_ProductionItem; // Item that is produced by this building.
     public float m_ProductionTime;
+    public float m_ProductionRate;
+    public float m_ProductionTime_Elves;
 
-
+    
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //m_ProductionRate = 60 / m_ProductionTime + 60/(2*m_ProductionTime)* m_CurrentElves;  prodcution rate in fps??
+        //m_ProductionTime_Elves = 60/m_ProductionRate  production time after applying elves
+        if (m_IsProducing)
+        {
+            m_CurrentProgress = m_CurrentProgress - 1;
+        }
+
+        if (m_CurrentProgress == 0)
+        {
+            m_CurrentProgress = m_ProductionTime_Elves;
+            //instantiate m_ProductionItem at next conveyer in cycle (4 possible outputs)  %%daniel
+        }
+
     }
 
     // Update is called once per frame
