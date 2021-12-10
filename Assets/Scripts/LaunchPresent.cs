f5af8d12b9a3449a652fbd079773344109ab8f2b
Dan <daniel@ac.com.au>
2021-12-11 07:55:01 +1100

    Gift canon now earns money and launches off finished presents.

---
+++ Assets/Scripts/LaunchPresent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchPresent : MonoBehaviour
+{
+    public Rigidbody2D m_Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody.AddForce(new Vector3(0.707f, 0.707f, 0) * 25, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Structures/GiftCanon.cs
+++ Assets/Scripts/Structures/GiftCanon.cs
@@ -26,15 +26,75 @@ public class GiftCanon : Structure
     public Sprite m_Fuse5;
 
     public SpriteRenderer m_OurRenderer;
-    
+
+    public GameObject m_PresentLaunchPrefab;
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
         BoxCollider2D ItemSort = gameObject.GetComponent<BoxCollider2D>();
-        if (ItemSort.tag == "Present") //check if right resource for level
+        if (collision.gameObject.tag == "Present") //check if right resource for level
         {
             m_CurrentResource = m_CurrentResource + 1; //adding 1 to the inventory
             ItemQueue.Add(collision.gameObject);
+
+
+
+            if (m_CurrentResource >= m_ResourceCost && !m_IsProducing)
+            {
+                m_IsProducing = true;
+
+                for (int i = ItemQueue.Count - 1; i >= ItemQueue.Count - m_CurrentResource; i--)
+                {
+                    GameObject.Destroy(ItemQueue[i]);
+                    ItemQueue.RemoveAt(i);
+                    m_CurrentResource--;
+                }
+
+                //m_CurrentResource = m_CurrentResource - m_ResourceCost;
+
+                //for (int i = 0; i < ItemQueue.Count; i++)
+                //{
+                //    if (ItemQueue[i].tag == "Wood1")
+                //    {
+                //        //add 10 to cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Wood2")
+                //    {
+                //        //add 20 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Wood3")
+                //    {
+                //        //add 40 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Plastic1")
+                //    {
+                //        //add 25 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Plastic2")
+                //    {
+                //        //add 50 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Plastic3")
+                //    {
+                //        //add 100 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Metal1")
+                //    {
+                //        //add 45 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Metal2")
+                //    {
+                //        //add 125 cash
+                //    }
+                //    else if (ItemQueue[i].tag == "Metal3")
+                //    {
+                //        //add 250 cash
+                //    }
+                //    GameObject.Destroy(ItemQueue[i]);
+                //}
+
+            }
+
         }
         //else
         //{
@@ -48,56 +108,11 @@ public class GiftCanon : Structure
     void Start()
     {
         //m_ProductionRate = 60 / m_ProductionTime + 60/(2*m_ProductionTime)* m_CurrentElves;  prodcution rate in fps?? calculates items per minute
+        m_IsProducing = false;
     }
     void Update()
     {
-        if (m_CurrentResource >= m_ResourceCost && !m_IsProducing)
-        {
-            m_IsProducing = true;
-            m_CurrentResource = m_CurrentResource - m_ResourceCost;
-
-            for (int i = 0; i < ItemQueue.Count; i++)
-            {
-                if (ItemQueue[i].tag == "Wood1")
-                {
-                    //add 10 to cash
-                }
-                else if (ItemQueue[i].tag == "Wood2")
-                {
-                    //add 20 cash
-                }
-                else if (ItemQueue[i].tag == "Wood3")
-                {
-                    //add 40 cash
-                }
-                else if (ItemQueue[i].tag == "Plastic1")
-                {
-                    //add 25 cash
-                }
-                else if (ItemQueue[i].tag == "Plastic2")
-                {
-                    //add 50 cash
-                }
-                else if (ItemQueue[i].tag == "Plastic3")
-                {
-                    //add 100 cash
-                }
-                else if (ItemQueue[i].tag == "Metal1")
-                {
-                    //add 45 cash
-                }
-                else if (ItemQueue[i].tag == "Metal2")
-                {
-                    //add 125 cash
-                }
-                else if (ItemQueue[i].tag == "Metal3")
-                {
-                    //add 250 cash
-                }
-                GameObject.Destroy(ItemQueue[i]);
-            }
-
-        }
+        
 
 
 
@@ -135,6 +150,9 @@ public class GiftCanon : Structure
                 //change to fuse 1
 
                 GameManager.s_Instance.m_Cash += 100;
+
+                GameObject newLaunchedPresent = GameObject.Instantiate(m_PresentLaunchPrefab);
+                newLaunchedPresent.transform.position = transform.position;
                 
             }
         }
