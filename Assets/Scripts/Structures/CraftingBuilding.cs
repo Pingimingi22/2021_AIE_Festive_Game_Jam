1ccac46c57e53c0f4a029447504e2e5256aaa10d
Nicolas Furr <nicolasfurr@optusnet.com.au>
2021-12-10 12:34:10 +1100

    Crafting building logic

--- Assets/Scripts/Structures/CraftingBuilding.cs
+++ Assets/Scripts/Structures/CraftingBuilding.cs
@@ -6,6 +6,9 @@ public class CraftingBuilding : Structure
 {
     public int m_CurrentElves;
     public bool m_IsProducing;
+    public int m_ResourceCost;
+    public int m_CurrentResource;
+
 
     // connections struct todo.
 
@@ -14,16 +17,60 @@ public class CraftingBuilding : Structure
     public float m_ProductionTime;
     public float m_ProductionRate;
     public float m_ProductionTime_Elves;
+    public List<GameObject> ItemQueue = new List<GameObject>();
 
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        BoxCollider2D ItemSort = gameObject.GetComponent<BoxCollider2D>();
+        if (m_Level == 1 && ItemSort.tag == "Wood" ) //check if right resource for level
+        {
+            m_CurrentResource = m_CurrentResource + 1; //adding 1 to the inventory
+            ItemQueue.Add(collision.gameObject);
+        }
+        else
+        {
+            GameObject.Destroy(collision.gameObject);
+        }
+        if (m_Level == 2 && ItemSort.tag == "Plastic") //check if right resource for level
+        {
+            m_CurrentResource = m_CurrentResource + 1; //adding 1 to the inventory
+        }
+        else
+        {
+            GameObject.Destroy(collision.gameObject);
+        }
+        if (m_Level == 3 && ItemSort.tag == "Metal") //check if right resource for level
+        {
+            m_CurrentResource = m_CurrentResource + 1; //adding 1 to the inventory
+        }
+        else
+        {
+            GameObject.Destroy(collision.gameObject);
+        }
+    }
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //m_ProductionRate = 60 / m_ProductionTime + 60/(2*m_ProductionTime)* m_CurrentElves;  prodcution rate in fps??
-        //m_ProductionTime_Elves = 60/m_ProductionRate  production time after applying elves
+        //m_ProductionRate = 60 / m_ProductionTime + 60/(2*m_ProductionTime)* m_CurrentElves;  prodcution rate in fps?? calculates items per minute
+        //m_ProductionTime_Elves = 60/m_ProductionRate  production time in seconds after applying elves
+                                 
+
+        if (m_CurrentResource >= m_ResourceCost)
+        {
+            m_IsProducing = true;
+            m_CurrentResource = m_CurrentResource - m_ResourceCost;
+
+            for (int i = 0; i < ItemQueue.Count; i++)
+            {
+                GameObject.Destroy(ItemQueue[i]);
+            }
+            
+        }
+
         if (m_IsProducing)
         {
             m_CurrentProgress = m_CurrentProgress - 1;
@@ -32,7 +79,8 @@ public class CraftingBuilding : Structure
         if (m_CurrentProgress == 0)
         {
             m_CurrentProgress = m_ProductionTime_Elves;
-            //instantiate m_ProductionItem at next conveyer in cycle (4 possible outputs)  %%daniel
+            m_IsProducing = false;
+            //instantiate m_ProductionItem at next conveyer in cycle (3 possible outputs)  %%daniel
         }
 
     }
